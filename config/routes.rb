af329a40750dd713631fd9dc10f493146b8474e6
KK <k.koichi276052@gmail.com>
2023-09-26 17:19:15 +0900

    Finally worked with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "pages#home"
+  get "about", to: "pages#about"
+  # resources :articles, only: [:show]
+  resources :articles
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
+

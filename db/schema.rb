ecbe1f345c3b79366a792fc29062f90b5ef36aa7
片岡宏一 <kataokakoichi@Ks-MacBook-Air.local>
2023-10-31 16:21:35 +0900

    add user validation

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema[7.0].define(version: 2023_10_26_020441) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|

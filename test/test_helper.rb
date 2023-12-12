32f1b454305f8ab48ec09bdc560b16e4d1d444d0
KK <k.koichi276052@gmail.com>
2023-12-12 17:18:19 +0900

    only admin can add category

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,9 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def sign_as_user(user)
+    post login_path, params: { session: { emial: user.email, password: "password"} }
+  end
+
   # Add more helper methods to be used by all tests here...
 end
